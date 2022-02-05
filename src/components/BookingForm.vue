59097edf9332d0c4af36f9f1beb01407f9cf0781
Paweł Rosolowski <prosolowski@whitelabelcoders.com>
2022-02-05 20:28:45 +0100

    Initial state - closed

---
+++ src/components/BookingForm.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="booking-form">
+      <div class="booking-form__price">298 zł</div>
+      <div class="booking-form__rate"><StarRating :rate="4.5" :reviews="123" /></div>
+      <div class="booking-form__btn">
+        <button type="button" class="btn btn-green">Reserve</button>
+      </div>
+      <div class="booking-form__input">
+        <div class="date-range">
+          <input type="text" class="date-range__from" placeholder="Date from">
+          <IconArrowRight />
+          <input type="text" class="date-range__to" placeholder="Date to">
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import StarRating from './StarRating.vue'
+import IconArrowRight from './IconArrowRight.vue'
+
+export default {
+  name: 'BookingForm',
+  props: {},
+  components: {
+    StarRating,
+    IconArrowRight
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600;700&display=swap');
+.booking-form {
+  display: grid;
+  width: 20.25rem;
+  height: 8.5rem;
+  padding: 1.5rem;
+  background: #FFFFFF;
+  box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+  display: grid; 
+  grid-template-columns: 1fr 1fr; 
+  grid-template-rows: 1.5rem 1.5rem 1fr;
+  gap: 0px 0px;
+  grid-template-areas: 
+    "leftTop rightTop"
+    "leftBottom rightTop"
+    "bottom bottom";
+  font-family: Montserrat;
+  font-style: normal;
+  color: #333333;
+  &__price { 
+    grid-area: leftTop;
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    display: flex;
+    align-items: flex-end;
+  }
+  &__rate {
+    grid-area: leftBottom;
+    display: flex;
+    align-items: flex-end;
+  }
+  &__btn {
+    grid-area: rightTop;
+    display: flex;
+    justify-content: flex-end;
+    .btn-green {
+      font-family: Montserrat;
+      font-weight: 600;
+      font-size: 0.875rem;
+      line-height: 1.375rem;
+      color: #FFFFFF;
+      background-color: #4E9845;
+      min-width: 6.375rem;
+      height: 3rem;
+      border: none;
+      border-radius: 3rem;
+      cursor: pointer;
+      &:hover {
+        background-color: #46873e;
+      }
+    }
+  }
+  &__input { 
+    grid-area: bottom;
+    display: flex;
+    align-items: center;
+    .date-range {
+      width: 100%;
+      height: 3rem;
+      border: 1px solid #A6CBA2;
+      box-sizing: border-box;
+      border-radius: 3rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      input {
+        cursor: pointer;
+        width: 100%;
+        border: none;
+        border-radius: 3rem;
+        padding: 8px;
+        margin: 8px;
+        &::placeholder {
+          font-family: Montserrat;
+          font-weight: 600;
+          font-size: 0.875rem;
+          line-height: 1.25rem;
+          color: #999999;
+        }
+        &:hover {
+          background: #EDF5EC;
+          &::placeholder {
+            color: #4E9845;
+          }
+        }
+        &:focus-visible {
+          background: #EDF5EC;
+          outline-color: #EDF5EC;
+          caret-color: transparent;
+          &::placeholder {
+            color: #4E9845;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
