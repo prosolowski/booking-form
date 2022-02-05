8cdcf07705134d219f97c896da11a5c4cf7be7d7
Paweł Rosolowski <prosolowski@whitelabelcoders.com>
2022-02-05 14:27:25 +0100

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BookingCalendar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BookingCalendar from './components/BookingCalendar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BookingCalendar
   }
 }
 </script>
---
+++ src/components/BookingCalendar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="booking-calendar">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookingCalendar',
+  props: {}
+}
+</script>
+
+<style scoped>
+
+</style>
